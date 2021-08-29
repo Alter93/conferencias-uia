86870dff3faad58df1c9d87ba095d05f32ce88e9
Alter93 <alexae93@gmail.com>
2021-08-29 11:41:50 -0500

    cambiar mayusculas

---
+++ static/js/meeting.js
@@ -0,0 +1,98 @@
+window.addEventListener('DOMContentLoaded', function(event) {
+  console.log('DOM fully loaded and parsed');
+
+  websdkready();
+});
+
+var signature = document.getElementsByName("signature")[0].getAttribute('content');
+var apiKey = document.getElementsByName("apiKey")[0].getAttribute('content');
+var meetingNumber = document.getElementsByName("meetingNumber")[0].getAttribute('content');
+var userName = document.getElementsByName("userName")[0].getAttribute('content');
+var passWord = document.getElementsByName("passWord")[0].getAttribute('content');
+var userEmail = document.getElementsByName("userEmail")[0].getAttribute('content');
+
+function websdkready() {
+  var testTool = window.testTool;
+  // get meeting args from url
+  var tmpArgs = testTool.parseQuery();
+  var meetingConfig = {
+    apiKey: apiKey,
+    meetingNumber: meetingNumber,
+    userName: userName,
+    passWord: passWord,
+    userEmail: userEmail,
+    signature: signature,
+
+    leaveUrl: "/index",
+    role: 0,
+    lang: 'es-ES',
+    china: 0,
+  };
+
+  // a tool use debug mobile device
+  if (testTool.isMobileDevice()) {
+    vConsole = new VConsole();
+  }
+  console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+
+  // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
+  // ZoomMtg.setZoomJSLib("https://source.zoom.us/1.9.8/lib", "/av"); // CDN version defaul
+
+  ZoomMtg.prepareJssdk();
+  function beginJoin(signature) {
+    ZoomMtg.init({
+      leaveUrl: meetingConfig.leaveUrl,
+      webEndpoint: meetingConfig.webEndpoint,
+      disableCORP: !window.crossOriginIsolated, // default true
+      // disablePreview: false, // default false
+      success: function () {
+        console.log(meetingConfig);
+        console.log("signature", signature);
+        ZoomMtg.i18n.load(meetingConfig.lang);
+        ZoomMtg.i18n.reload(meetingConfig.lang);
+        ZoomMtg.join({
+          meetingNumber: meetingConfig.meetingNumber,
+          userName: meetingConfig.userName,
+          signature: signature,
+          apiKey: meetingConfig.apiKey,
+          userEmail: meetingConfig.userEmail,
+          passWord: meetingConfig.passWord,
+          success: function (res) {
+            console.log("join meeting success");
+            console.log("get attendeelist");
+            ZoomMtg.getAttendeeslist({});
+            ZoomMtg.getCurrentUser({
+              success: function (res) {
+                console.log("success getCurrentUser", res.result.currentUser);
+              },
+            });
+          },
+          error: function (res) {
+            console.log(res);
+          },
+        });
+      },
+      error: function (res) {
+        console.log(res);
+      },
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserJoin', function (data) {
+      console.log('inMeetingServiceListener onUserJoin', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserLeave', function (data) {
+      console.log('inMeetingServiceListener onUserLeave', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onUserIsInWaitingRoom', function (data) {
+      console.log('inMeetingServiceListener onUserIsInWaitingRoom', data);
+    });
+
+    ZoomMtg.inMeetingServiceListener('onMeetingStatus', function (data) {
+      console.log('inMeetingServiceListener onMeetingStatus', data);
+    });
+  }
+
+  beginJoin(signature);
+};
