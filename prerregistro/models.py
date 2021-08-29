b7898f42fd855daf148a9da8c5680936334ed608
Alter93 <alexae93@gmail.com>
2021-08-29 11:59:24 -0500

    excepciones

--- prerregistro/models.py
+++ prerregistro/models.py
@@ -18,9 +18,16 @@ class Prerregistro(models.Model):
 
     def save(self, *args, **kwargs):
         usuario = User.objects.create_user(slugify(self.nombre + "-" + self.apellido) , self.email, 'W3lcome2!')
-        usuario.save()
-        self.usuario_db = usuario
-        super().save(*args, **kwargs)
+        success = True
+        try:
+            usuario.save()
+
+        except Exception e:
+            success = False
+
+        if success:
+            self.usuario_db = usuario
+            super().save(*args, **kwargs)
 
     class Meta:
         db_table = "prerregistro"
