75129a9c4dbe71c56eaceca0fb5c767224cbece3
Alter93 <alexae93@gmail.com>
2021-09-02 17:23:01 -0500

    Login

--- presentaciones/views.py
+++ presentaciones/views.py
@@ -9,39 +9,42 @@ def home(request):
     if request.user.is_authenticated:
         return render(request, 'home.html', {})
     else:
-        return render(request, 'home.html', {})
-        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
 def horarios(request):
     if request.user.is_authenticated:
         return render(request, 'horarios.html', {})
     else:
-        return render(request, 'horarios.html', {})
-        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
 def ponentes(request):
     if request.user.is_authenticated:
         return render(request, 'ponentes.html', {})
     else:
-        return render(request, 'ponentes.html', {})
-        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
 def conferencia(request, conf_uid):
-    zoom = ConferenciaZoom(conf = conf_uid)
-    print(zoom.conferencia.titulo)
-    return render(request, 'conferencia/meeting.html', {
-        'signature': zoom.signature,
-        'apiKey': ConferenciaZoom.API_KEY,
-        'meetingNumber': zoom.conferencia.zoom_id,
-        'userName': 'Alejandro',
-        'userEmail': 'aaaa@gmail.com',
-        'passWord': zoom.conferencia.password_zoom,
-        'redirect': request.path
-    })
+    if request.user.is_authenticated:
+        zoom = ConferenciaZoom(conf = conf_uid)
+        print(zoom.conferencia.titulo)
+        return render(request, 'conferencia/meeting.html', {
+            'signature': zoom.signature,
+            'apiKey': ConferenciaZoom.API_KEY,
+            'meetingNumber': zoom.conferencia.zoom_id,
+            'userName': 'Alejandro',
+            'userEmail': 'aaaa@gmail.com',
+            'passWord': zoom.conferencia.password_zoom,
+            'redirect': request.path
+        })
+    else:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
 def visitar_sala(request, id_sala):
-    conferencia = ConferenciaZoom(lugar = id_sala).conferencia
-    return render(request, 'sala.html', {
-        'conf': conferencia,
-        'lugar': id_sala,
-    })
+    if request.user.is_authenticated:
+        conferencia = ConferenciaZoom(lugar = id_sala).conferencia
+        return render(request, 'sala.html', {
+            'conf': conferencia,
+            'lugar': id_sala,
+        })
+    else:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
