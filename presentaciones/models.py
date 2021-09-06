fa17b06f3f0da4212e63199f483edacb2742007e
Alter93 <alexae93@gmail.com>
2021-09-06 18:11:45 -0500

    campos no obligatorios

--- presentaciones/models.py
+++ presentaciones/models.py
@@ -6,20 +6,20 @@ class Conferencista(models.Model):
     id = models.BigAutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
-    biografia = models.CharField(max_length=2000)
+    biografia = models.CharField(max_length=2000, null=True, blank=True)
     def __str__(self):
         return '%s %s' % (self.nombre, self.apellido)
 
 class Conferencia(models.Model):
     id = models.BigAutoField(primary_key=True)
     titulo = models.CharField(max_length=200)
-    descripcion = models.CharField(max_length=1000)
+    descripcion = models.CharField(max_length=1000,null=True, blank=True)
     fecha_hora = models.DateTimeField()
     duracion = models.DurationField()
     lugar = models.CharField(max_length=200)
     zoom_id = models.CharField(max_length=200)
     password_zoom = models.CharField(max_length=200, null=True, blank=True)
-    id_conferencista = models.ForeignKey('Conferencista', on_delete=models.CASCADE)
+    id_conferencista = models.ForeignKey('Conferencista', on_delete=models.CASCADE, null=True, blank=True)
     uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
 
     def __str__(self):
